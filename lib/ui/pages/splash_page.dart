47fd581b86d18d64c470c99ef5f33cdd875b3725
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-02-02 19:01:21 +0800

    complete splash screen and onboarding

---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:ipay/shared/theme.dart';
+import 'package:ipay/ui/pages/onboarding_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnboardingPage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Container(
+          width: 155,
+          height: 50,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/logo.png',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
